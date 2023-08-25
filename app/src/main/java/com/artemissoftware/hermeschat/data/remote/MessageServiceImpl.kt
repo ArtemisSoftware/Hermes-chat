7b418ade0faf77ac87ba491d17f641e77f60f35a
gustavo <gustavo.isaac@gmail.com>
2023-08-25 04:36:04 +0100

    MessageService

---
+++ app/src/main/java/com/artemissoftware/hermeschat/data/remote/MessageServiceImpl.kt
@@ -0,0 +1,21 @@
+package com.artemissoftware.hermeschat.data.remote
+
+import com.artemissoftware.hermeschat.data.remote.dto.MessageDto
+import com.artemissoftware.hermeschat.data.remote.mappers.toMessage
+import com.artemissoftware.hermeschat.domain.model.Message
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+
+class MessageServiceImpl(
+    private val client: HttpClient,
+) : MessageService {
+
+    override suspend fun getAllMessages(): List<Message> {
+        return try {
+            client.get<List<MessageDto>>(MessageService.Endpoints.GetAllMessages.url)
+                .map { it.toMessage() }
+        } catch (e: Exception) {
+            emptyList()
+        }
+    }
+}
