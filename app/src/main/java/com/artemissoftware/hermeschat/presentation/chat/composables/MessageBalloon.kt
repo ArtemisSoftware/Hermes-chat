9e83a2390319825880f6cb5767e6fd221dfca631
gustavo <gustavo.isaac@gmail.com>
2023-08-29 05:10:02 +0100

    Ui fixes

--- app/src/main/java/com/artemissoftware/hermeschat/presentation/chat/composables/MessageBalloon.kt
+++ app/src/main/java/com/artemissoftware/hermeschat/presentation/chat/composables/MessageBalloon.kt
@@ -2,9 +2,13 @@ package com.artemissoftware.hermeschat.presentation.chat.composables
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -13,10 +17,16 @@ import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.artemissoftware.hermeschat.R
 import com.artemissoftware.hermeschat.domain.model.Message
+import com.artemissoftware.hermeschat.ui.theme.GrayFade
+import com.artemissoftware.hermeschat.ui.theme.HermesChatTheme
+import com.artemissoftware.hermeschat.ui.theme.Pink
 
 @Composable
 fun MessageBalloon(
@@ -24,22 +34,27 @@ fun MessageBalloon(
     message: Message,
     modifier: Modifier = Modifier,
 ) {
+    val backgroundColor = if (isOwnMessage) Pink else MaterialTheme.colorScheme.secondary
+    val usernameColor = if (isOwnMessage) GrayFade else Color.Gray
+
     Column(
         modifier = modifier
             .drawBehind {
-                balloon(isOwnMessage)
+                balloon(isOwnMessage, backgroundColor = backgroundColor)
             }
             .background(
-                color = if (isOwnMessage) Color.Green else Color.DarkGray,
+                color = backgroundColor,
                 shape = RoundedCornerShape(10.dp),
             )
             .padding(8.dp),
     ) {
         Text(
-            text = message.username,
+            text = if (isOwnMessage) stringResource(id = R.string.you) else message.username,
             fontWeight = FontWeight.Bold,
-            color = Color.White,
+            fontSize = 12.sp,
+            color = usernameColor,
         )
+        Spacer(modifier = Modifier.height(2.dp))
         Text(
             text = message.text,
             color = Color.White,
@@ -54,6 +69,7 @@ fun MessageBalloon(
 
 private fun DrawScope.balloon(
     isOwnMessage: Boolean,
+    backgroundColor: Color,
 ) {
     val cornerRadius = 10.dp.toPx()
     val triangleHeight = 20.dp.toPx()
@@ -74,36 +90,43 @@ private fun DrawScope.balloon(
     }
     drawPath(
         path = trianglePath,
-        color = if (isOwnMessage) Color.Green else Color.DarkGray,
+        color = backgroundColor,
     )
 }
 
 @Preview(showBackground = true)
 @Composable
 private fun MessageBalloon_isOwnMessage_true_Preview() {
-    MessageBalloon(
-        isOwnMessage = true,
-        message = Message(
-            text = "First message",
-            formattedTime = "12-03-2009",
-            username = "username",
-        ),
-        modifier = Modifier
-            .width(200.dp),
-    )
+    HermesChatTheme {
+        MessageBalloon(
+            isOwnMessage = true,
+            message = Message(
+                text = "First message",
+                formattedTime = "12-03-2009",
+                username = "username",
+            ),
+            modifier = Modifier
+                .width(200.dp),
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 private fun MessageBalloon_isOwnMessage_false_Preview() {
-    MessageBalloon(
-        isOwnMessage = false,
-        message = Message(
-            text = "First message",
-            formattedTime = "12-03-2009",
-            username = "username",
-        ),
-        modifier = Modifier
-            .width(200.dp),
-    )
+    HermesChatTheme {
+        Column(modifier = Modifier.fillMaxWidth()) {
+            MessageBalloon(
+                isOwnMessage = false,
+                message = Message(
+                    text = "First message",
+                    formattedTime = "12-03-2009",
+                    username = "username",
+                ),
+                modifier = Modifier
+                    .width(200.dp),
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+        }
+    }
 }
--- app/src/main/java/com/artemissoftware/hermeschat/ui/theme/Color.kt
+++ app/src/main/java/com/artemissoftware/hermeschat/ui/theme/Color.kt
@@ -6,3 +6,4 @@ val Dark300 = Color(0xff20232b)
 val Dark500 = Color(0xff15171c)
 
 val Pink = Color(0xffb785f6)
+val GrayFade = Color(0xffeeeeee)
