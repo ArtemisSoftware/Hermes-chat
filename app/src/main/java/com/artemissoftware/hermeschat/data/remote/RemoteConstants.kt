6595063730f5ee9febd84d9ef0034ed4f42152bf
gustavo <gustavo.isaac@gmail.com>
2023-08-29 04:16:41 +0100

    Colors and theme

--- app/src/main/java/com/artemissoftware/hermeschat/data/remote/RemoteConstants.kt
+++ app/src/main/java/com/artemissoftware/hermeschat/data/remote/RemoteConstants.kt
@@ -2,8 +2,8 @@ package com.artemissoftware.hermeschat.data.remote
 
 object RemoteConstants {
 
-    private const val IP = "192.168.0.2"
-    private const val PORT = "8082"
+    private const val IP = "192.168.1.91"
+    private const val PORT = "8081"
 
     const val BASE_URL = "http://$IP:$PORT"
     const val SOCKET_BASE_URL = "ws://$IP:$PORT"
